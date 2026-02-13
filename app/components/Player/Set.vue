072215a008f8b8de3514d6b27ed411541a9eb90c
Nico Wu <>
2026-02-13 10:42:00 +0800

    rebuild:重建PlayerSet, PlayerSetOrderMusic

--- app/components/Player/Set.vue
+++ app/components/Player/Set.vue
@@ -1,9 +1,177 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import player_logo from '~/assets/img/iscim_player_logo.png';
+
+const musicStore = useMusicStore();
+const player = usePlayer();
+const {removeMusicFromSet, loadMusicSet, addToLists} = useMusicSets();
+const {uiState, currentItem, fieldItems, openOptions, openInfo} = useMusicDetail();
+
+const musicSubOptions = [
+  {title: 'add_to_play_list', id: 'next', icon: 'plus'},
+  {title: 'remove_from_set', id: 'removeFromSet', icon: 'delete-o'}
+];
+
+const currentMusicList = computed(() => {
+  if (!musicStore.currentSet || !musicStore.currentSet.content) return false;
+  return musicStore.currentSet.content;
+})
+
+const hasMusic = computed(() => {
+  if (!musicStore.currentSet || !musicStore.currentSet.content) return false;
+  return musicStore.currentSet.content.length > 0;
+});
+
+onMounted(() => {
+  if (!musicStore.currentSet ) return;
+  if (Array.isArray(musicStore.currentSet.content)) return;
+})
+
+const handleMusicAction = (actionType: string) => {
+  console.log(actionType)
+  if (!currentItem.value) return;
+
+  switch(actionType) {
+    case 'next':
+      addToLists(currentItem.value);
+      showSuccessToast('Add to queue');
+      break;
+    case 'removeFromSet':
+      removeMusicFromSet(currentItem.value);
+      break;
+  };
+  uiState.showOptions = false;
+};
+
+const handlePlayMusic = (musicItem: any) => {
+  musicStore.resetMusic();
+  if (!musicStore.currentSet) return;
+  loadMusicSet(musicStore.currentSet);
+  const index = musicStore.queue.findIndex((m:any) => m.src === musicItem.src);
+  if (index === -1) return;
+  player.setSourceByIndex(index);
+  musicStore.setPlayerQueue(true);
+};
+
+const formatIndex = (index: number) => {
+  return String(index + 1).padStart(2, '0');
+};
+
+const handleAddMusic = () => {
+  musicStore.setPlayerSetOrderMusic(true);
+};
+</script>
 
 <template>
   <div>
-    Component: Player/Set
+    <CommonSubHeader 
+      :title="musicStore.currentSet?.name"
+      left-icon="arrow-down"
+      @click-left="musicStore.setPlayerSet(false)"
+    />
+    <div class="set-container">
+      <div class="cover-wrapper">
+        <van-image :src="player_logo" />
+      </div>
+
+      <div class="tab-controls">
+        <van-row class="control-bar custom-button">
+          <van-col>
+            <van-button icon="play" text="播放清單" />
+          </van-col>
+          <van-col>
+            <van-button icon="add-square" text="新增音樂" @click="handleAddMusic" />
+          </van-col>
+        </van-row>
+      </div>
+
+      <van-divider />
+
+      <div class="set-wrapper custom-button">
+        <van-button v-if="!hasMusic" @click="handleAddMusic">{{ $t('add_some_music') }}</van-button>
+        <van-list v-else>
+          <van-cell
+            v-for="(item, index) in currentMusicList"
+            :key="index"
+            @click="handlePlayMusic(item)"
+          >
+            <template #title>
+              <span class="index-number">{{ formatIndex(Number(index)) }}</span>
+              <span class="item-name">{{ `${item.name} - ${item.chakra}` }}</span>
+            </template>
+            <template #label>
+              <van-text-ellipsis
+                :content="String(item.intro)"
+                expand-text=""
+                collapse-text=""
+              />
+            </template>
+            <template #right-icon>
+              <van-icon 
+                name="ellipsis" 
+                size="20" 
+                @click.stop="openOptions(item)" 
+              />
+            </template>
+          </van-cell>
+        </van-list>
+      </div>
+    </div>
+    <CommonActionMenuPopup 
+      v-model:show="uiState.showOptions"
+      :title="currentItem?.name"
+      :actions="musicSubOptions"
+      @select="handleMusicAction"
+      @info="openInfo()"
+    />
+    <CommonInfoDetailPopup 
+      v-model:show="uiState.showInfo"
+      :field-items="fieldItems"
+    />
   </div>
+
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.cover-wrapper {
+  display: flex;
+  justify-content: center;
+
+  .van-image {
+    width: 240px;
+    height: 240px;
+  }
+}
+
+.control-bar {
+  justify-content: space-evenly;
+}
+
+.set-wrapper {
+  overflow-y: auto;
+  height: 40vh;
+
+  &.custom-button {
+    text-align: center;
+    .van-button {
+      width: 76%;
+    }
+  }
+
+  .van-cell {
+    height: 60px;
+  }
+
+  :deep(.van-cell__title) {
+    text-align: left;
+  }
+}
+
+.index-number {
+  margin-right: 10px;
+  font-variant-numeric: tabular-nums;
+}
+
+.van-popup {
+  overflow-y: hidden;
+}
+</style>
--- app/components/Player/SetOrderMusic.vue
+++ app/components/Player/SetOrderMusic.vue
@@ -1,9 +1,206 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const musicStore = useMusicStore();
+const { updateSetToDb } = useDataBase();
+
+const activeTab = ref(0);
+const showPickerChakras = ref(false);
+const pickerValue = ref([]);
+const selectedItem = ref<any>();
+
+const subTabActive = reactive<Record<string, number>>({
+  system: 0,
+  custom: 0
+});
+
+const musicTabs = computed(() => [
+  {
+    key: 'system',
+    name: 'Menu.system_music',
+    data: musicStore.subMusic
+  },
+  {
+    key: 'custom',
+    name: 'Menu.custom_music',
+    data: musicStore.subMusicUpdated
+  }
+]);
+
+const subChakra = computed(() => [
+  { text: $t("Chakra.balance"), idx: 0, value: "Balance" },
+  { text: $t("Chakra.root"), idx: 1, value: "Root" },
+  { text: $t("Chakra.sacral"), idx: 2, value: "Sacral" },
+  { text: $t("Chakra.navel"), idx: 3, value: "Navel" },
+  { text: $t("Chakra.waist"), idx: 4, value: "Waist" },
+  { text: $t("Chakra.solar_plexus"), idx: 5, value: "Solar Plexus" },
+  { text: $t("Chakra.heart"), idx: 6, value: "Heart" },
+  { text: $t("Chakra.pineal"), idx: 7, value: "Pineal" },
+  { text: $t("Chakra.third_eye"), idx: 8, value: "Third Eye" },
+  { text: $t("Chakra.crown"), idx: 9, value: "Crown" },
+]);
+
+const handleSelectChakra = (item: any) => {
+  selectedItem.value = item;
+  showPickerChakras.value = true;
+};
+
+const handleAddToSet = async(item: any) => {
+  const _set = musicStore.currentSet;
+  if (!_set || !_set.id) return;
+  if (!Array.isArray(_set.content)) _set.content = [];
+  _set.content.push(item);
+  const res = await updateSetToDb (_set.id, { content: _set.content});
+
+  if (res) showSuccessToast($t("Toast.add_successfully"));
+};
+
+const handleNextStep = () => {
+  console.log('Next')
+};
+
+const handleCancel = () => {
+  musicStore.setPlayerSetOrderMusic(false);
+};
+
+const onConfirmChakra = (res: any) => {
+  showPickerChakras.value = false;
+  pickerValue.value = res.selectedValues;
+  selectedItem.value.chakra = res.selectedOptions[0].idx;
+};
+
+onMounted(() => {
+  if(!musicStore.isPro) return;
+  musicStore.initMusicData();
+});
+
+</script>
 
 <template>
   <div>
-    Component: Player/SetOrderMusic
+    <CommonSubHeader 
+      :title="musicStore.currentSet?.name"
+      left-icon="arrow-left"
+      @click-left="musicStore.setPlayerSetOrderMusic(false)"
+    />
+    <van-tabs
+      v-model:active="activeTab"
+      class="custom-tab"
+      sticky
+      type="card"
+    >
+      <van-tab
+        v-for="tab in musicTabs"
+        :key="tab.key"
+        :name="tab.name"
+        :title="$t(tab.name)"
+      >
+        <van-tabs
+          v-model:active="subTabActive[tab.key]"
+          type="card"
+          class="inner-tabs"
+        >
+          <van-tab
+            v-for="(subMusic, subIdx) in tab.data"
+            :key="subIdx"
+            :title="$t(subMusic.name)"
+          >
+            <div class="scrollable-list">
+              <van-cell-group inset>
+                <van-cell
+                  v-for="(item, itemIdx) in subMusic.menu"
+                  :key="itemIdx"
+                  :title="item.name"
+                  clickable
+                >
+                  <template #label>
+                    <van-text-ellipsis
+                      rows="1"
+                      :content="String(item.intro)"
+                      expand-text=""
+                      collapse-text=""
+                    />
+                  </template>
+                  <template #right-icon>
+                    <van-button 
+                      size="small"
+                      @click="handleSelectChakra(item)"
+                    >
+                      {{ item.chakra }}
+                    </van-button>
+                    <van-button
+                      size="small"
+                      icon="add-o"
+                      @click="handleAddToSet(item)"
+                    />
+                  </template>
+                </van-cell>
+              </van-cell-group>
+            </div>
+          </van-tab>
+        </van-tabs>
+      </van-tab>      
+    </van-tabs>
+    <van-action-bar>
+      <van-action-bar-icon 
+        icon="close" text="cancel" 
+        @click="handleCancel" 
+      />
+      <van-action-bar-icon 
+        icon="arrow-double-right" text="next" 
+        @click="handleNextStep" 
+      />
+    </van-action-bar>
+
+    <van-popup v-model:show="showPickerChakras" destroy-on-close round position="bottom">
+      <van-picker
+        :model-value="pickerValue"
+        :columns="subChakra"
+        @cancel="showPickerChakras = false"
+        @confirm="onConfirmChakra"
+      />
+    </van-popup>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use 'sass:color';
+.van-action-bar-icon {
+  flex: 1;
+}
+
+:deep(.van-popup) {
+  position: absolute;
+}
+
+.scrollable-list {
+  overflow-y: auto;
+  padding-bottom: 10px;
+  height: calc(100dvh - var(--van-tabs-card-height)*2 - var(--header-h) - var(--tabbar-h) - var(--sat) - var(--sab));
+
+  .van-cell-group--inset {
+    --van-cell-group-inset-padding: 0;
+  }
+}
+
+.showMiniBar .scrollable-list {
+  height: calc(100dvh - var(--van-tabs-card-height)*2 - var(--header-h) - var(--tabbar-h) - var(--minibar-h) - var(--sat) - var(--sab));
+}
+
+.van-cell-group--inset {
+  --van-cell-group-background: transparent;
+}
+
+.van-cell--clickable {
+  --van-cell-background: transparent;
+  align-items: center;
+
+  &.selected {
+    color: $color-text-tertiary;
+    font-weight: bold;
+  }
+}
+
+.van-cell--clickable:active {
+  background-color: transparent;
+}
+
+</style>
